9769bc9bbf4a2cf42446a8466694800777f2ffbd
Bonderukk <matusmatonibednarik@gmail.com>
2024-12-29 13:16:59 +0100

    Added favicons

--- vite.config.js
+++ vite.config.js
@@ -4,25 +4,62 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/zaverecne-zadanie~xbenacka//",
+  base: "https://webte1.fei.stuba.sk/~xbednarikm3/final/",
   plugins: [react(), VitePWA({
     registerType: 'autoUpdate',
-    injectRegister: false,
-
-    pwaAssets: {
-      disabled: false,
-      config: true,
-    },
-
+    injectRegister: 'auto',
+    includeAssets: ['favicon_io/*'],
     manifest: {
-      name: 'Záverečné zadanie',
-      short_name: 'ZZ',
+      name: 'FEI Jump',
+      short_name: 'FEI Jump',
       description: 'Záverečné zadanie na predmet Webové Technológie 1 - Matúš Bednařík, Cyril Beňačka',
       theme_color: '#ffffff',
+      background_color: '#ffffff',
+      display: 'standalone',
+      scope: '/',
+      start_url: '/',
+      orientation: 'portrait',
+      icons: [
+        {
+          src: 'favicon_io/favicon-16x16.png',
+          sizes: '16x16',
+          type: 'image/png'
+        },
+        {
+          src: 'favicon_io/favicon-32x32.png',
+          sizes: '32x32',
+          type: 'image/png'
+        },
+        {
+          src: 'favicon_io/android-chrome-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: 'favicon_io/android-chrome-512x512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        },
+        {
+          src: 'favicon_io/apple-touch-icon.png',
+          sizes: '180x180',
+          type: 'image/png'
+        },
+        {
+          src: 'favicon_io/apple-touch-icon-167x167.png',
+          sizes: '167x167',
+          type: 'image/png'
+        },
+        {
+          src: 'favicon_io/apple-touch-icon-152x152.png',
+          sizes: '152x152',
+          type: 'image/png'
+        }
+      ]
     },
 
     workbox: {
-      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+      globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
       cleanupOutdatedCaches: true,
       clientsClaim: true,
     },
