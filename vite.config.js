64285d4346cd77226353c1d220f5b5f216cecb4b
EkonC <cyril.benacka@gmail.com>
2024-12-29 15:19:04 +0100

    Adjustments in order to fix the site update problem

--- vite.config.js
+++ vite.config.js
@@ -59,7 +59,7 @@ export default defineConfig({
     },
 
     workbox: {
-      globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+      globPatterns: ['**/*.{js,css,html,ico,png,svg,json}'],
       cleanupOutdatedCaches: true,
       clientsClaim: true,
     },
